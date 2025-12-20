28ae59c7b6467d72fe8694b9a20ef8a2d425e5e5
Sune Simonsen <sune@we-knowhow.dk>
2025-12-20 10:57:17 +0100

    Tweaking

--- dot_config/nvim/plugin/editing.lua
+++ dot_config/nvim/plugin/editing.lua
@@ -1,9 +1,5 @@
 require('mini.completion').setup {
-  delay = {
-    completion = 10 ^ 10,
-    info = 500,
-    signature = 500,
-  },
+  delay = { info = 500, signature = 500 },
   lsp_completion = {
     snippet_insert = function(snippet)
       vim.snippet.expand(snippet)
